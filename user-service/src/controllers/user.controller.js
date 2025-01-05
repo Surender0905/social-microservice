258aa43c4ab84ef07c754bd501dbe87a972fdf88
Surender Pratap Singh <rk31151@gmail.com>
2025-01-05 19:11:48 +0530

    user part done

--- user-service/src/controllers/user.controller.js
+++ user-service/src/controllers/user.controller.js
@@ -1,8 +1,13 @@
 const { catchAsync, AppError } = require("../middlewares/errorHandler");
 const logger = require("../utils/logger");
 const User = require("../models/User");
-const { validate, registerSchema } = require("../middlewares/validation");
+const {
+    validate,
+    registerSchema,
+    loginSchema,
+} = require("../middlewares/validation");
 const generateToken = require("../utils/generateToken");
+const RefreshToken = require("../models/RefreshToken");
 
 const registerUser = catchAsync(async (req, res) => {
     logger.info("register user controller called");
@@ -35,6 +40,116 @@ const registerUser = catchAsync(async (req, res) => {
     await generateToken(res, user, "User registered successfully");
 });
 
+//login
+const login = catchAsync(async (req, res) => {
+    logger.info("login user controller called");
+
+    const validatedData = validate(loginSchema, req.body);
+
+    const user = await User.findOne({
+        $or: [
+            { email: validatedData.email.toLowerCase() },
+            { username: validatedData.username },
+        ],
+    });
+
+    if (!user) {
+        logger.warn("User not found");
+        throw new AppError("User not found", 400);
+    }
+
+    if (!user.comparePassword(validatedData.password)) {
+        logger.warn("Invalid password");
+        throw new AppError("Invalid password", 400);
+    }
+
+    await generateToken(res, user, "User logged in successfully");
+});
+
+//logout
+const logout = catchAsync(async (req, res) => {
+    logger.info("logout user controller called");
+
+    const refreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+    if (!refreshToken) {
+        logger.warn("Refresh token not found");
+        throw new AppError("Refresh token not found", 400);
+    }
+
+    //delete refresh token
+    await RefreshToken.findOneAndDelete({
+        token: refreshToken,
+    });
+
+    res.cookie("refreshToken", "", {
+        httpOnly: true,
+        sameSite: "strict",
+        expires: new Date(0),
+    });
+
+    res.status(200).json({
+        status: "success",
+        message: "User logged out successfully",
+    });
+});
+
+//forgot password
+const forgotPassword = catchAsync(async (req, res) => {});
+
+//reset password
+const resetPassword = catchAsync(async (req, res) => {});
+
+//refresh token
+const refreshToken = catchAsync(async (req, res) => {
+    logger.info("refresh token controller called");
+
+    const refreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+    if (!refreshToken) {
+        logger.warn("Refresh token not found");
+        throw new AppError("Refresh token not found", 400);
+    }
+
+    const refreshTokenDoc = await RefreshToken.findOne({
+        token: refreshToken,
+    });
+
+    if (!refreshTokenDoc) {
+        logger.warn("Refresh token not found");
+        throw new AppError("Refresh token not found", 400);
+    }
+
+    if (refreshTokenDoc.expiresAt < Date.now()) {
+        logger.warn("Refresh token expired");
+        throw new AppError("Refresh token expired", 400);
+    }
+
+    const user = await User.findById(refreshTokenDoc.userId);
+    if (!user) {
+        logger.warn("User not found");
+        throw new AppError("User not found", 400);
+    }
+    //delete refresh token
+    await RefreshToken.findOneAndDelete({
+        token: refreshToken,
+    });
+
+    ///delete old refresh token from db and create new one
+
+    await generateToken(res, user, "Refresh token refreshed successfully");
+});
+
 module.exports = {
     registerUser,
+
+    login,
+
+    logout,
+
+    forgotPassword,
+
+    resetPassword,
+
+    refreshToken,
 };
--- user-service/src/routes/user.route.js
+++ user-service/src/routes/user.route.js
@@ -1,7 +1,17 @@
 const express = require("express");
-const { registerUser } = require("../controllers/user.controller");
+const {
+    registerUser,
+    login,
+    logout,
+    refreshToken,
+    resetPassword,
+} = require("../controllers/user.controller");
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/refresh-token", refreshToken);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
